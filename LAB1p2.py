f7c466157854aa06b0077c7b84efc073ead6ac7a
princekumar66 <prince66.rj@gmail.com>
2022-01-31 17:25:05 +0530

    Finding Slope

---
+++ LAB1p2.py
@@ -0,0 +1,8 @@
+# Compute slope between points (x1, y1), (x2,y2)
+
+x1 = int(input("Enter the value for x1\n"))
+x2 = int(input("Enter the value for x2\n"))
+y1 = int(input("Enter the value for y1\n"))
+y2 = int(input("Enter the value for y2\n"))
+slope = (y2-y1)/(x2-x1)
+print("The value of Slope is : ", slope)
